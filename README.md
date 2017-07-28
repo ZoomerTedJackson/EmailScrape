644a3b8c82e8199aabe40f3cccf60cd01ac226fe
duckingtoniii <sirduckingtontheiii@gmail.com>
2017-07-27 19:01:54 -0700

    Init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# EmailScrape
 Simple script for finding emails in a text file and creating a list of them in a separate file
+
+### USAGE:
+Command Prompt*:
+
+run the emailscrape.py file and specify input and output files
+
+
+
+
+![image](https://cloud.githubusercontent.com/assets/7193260/20452099/2c7242ae-adb7-11e6-9e48-7ab7499eac07.png)
---
+++ emailscrape.py
@@ -0,0 +1,11 @@
+import re 
+
+dataname = raw_input('Input file > ')
+with open(dataname, "r") as myfile:
+    data = myfile.read().replace('\n', '')
+outputname = raw_input('Output file name > ')
+outputfile = open(outputname, "w")
+pattern = re.compile(r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])")
+for email in re.findall(pattern, data):
+	outputfile.write(email + "\n")
+outputfile.close()
